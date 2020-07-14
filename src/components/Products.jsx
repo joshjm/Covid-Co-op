2e0c129bcc4963d729ef9b16033f8069960dfe8b
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-07-14 12:24:59 +1000

    Usernames added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,27 +1,45 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import _ from "lodash";
 
 export class Products extends Component {
 
     constructor() {
         super();
         this.state = {
-            SERVER_URL: 'http://covid-co-op.herokuapp.com/products',
-            products: []
+            PRODUCT_URL: 'http://covid-co-op.herokuapp.com/products',
+            USER_URL: 'http://covid-co-op.herokuapp.com/users',
+            products: [],
+            users: []
         }
         this.fetchProducts = this.fetchProducts.bind(this);
+        this.fetchUsers = this.fetchUsers.bind(this);
         this.showProducts = this.showProducts.bind(this);
 
         this.fetchProducts();
+        this.fetchUsers();
     }
 
 
     // AXIOS CALL TO GET ALL PRODUCTS FROM THE SERVER
     fetchProducts() {
-        axios.get(this.state.SERVER_URL)
+        axios.get(this.state.PRODUCT_URL)
             .then(response => {
                 if (response.data) {
                     this.setState({products: response.data});
+                } else {
+                    this.setState({
+                        errors: response.data.errors
+                    })
+                }
+            })
+    }
+
+    fetchUsers() {
+        axios.get(this.state.USER_URL)
+            .then(response => {
+                if (response.data) {
+                    this.setState({ users: response.data.users });
                 } else {
                     this.setState({
                         errors: response.data.errors
@@ -40,7 +58,7 @@ export class Products extends Component {
                     <p>Category: {product.category}</p>
                     <p>Quanity available: {product.quantity}</p>
                     <p>{product.description}</p>
-                    <p>Provided by: {product.user}</p>
+                    <p>Provided by: {this.state.users[_.findIndex(this.state.users, {id: product.user_id})].name}</p>
                     <p>Uploaded at: {product.created_at}</p>
                 </div>
                 )
