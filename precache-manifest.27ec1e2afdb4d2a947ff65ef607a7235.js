3307be7f35d1c61f1c934a0f6be9e9af1fc4f214
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-07-15 12:07:48 +1000

    Updates

---
+++ precache-manifest.27ec1e2afdb4d2a947ff65ef607a7235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1616ba99965aefd27567121c5ce81baf",
+    "url": "/Covid-Co-op/index.html"
+  },
+  {
+    "revision": "74a3a6d5ace4057a7797",
+    "url": "/Covid-Co-op/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "54ce0be06c0ac14a34cb",
+    "url": "/Covid-Co-op/static/css/main.9df21531.chunk.css"
+  },
+  {
+    "revision": "74a3a6d5ace4057a7797",
+    "url": "/Covid-Co-op/static/js/2.90d05810.chunk.js"
+  },
+  {
+    "revision": "a43dc87748136f1e31bd8faae01dda0d",
+    "url": "/Covid-Co-op/static/js/2.90d05810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ce0be06c0ac14a34cb",
+    "url": "/Covid-Co-op/static/js/main.959c97c9.chunk.js"
+  },
+  {
+    "revision": "d3caf6ec0003b1ad318d",
+    "url": "/Covid-Co-op/static/js/runtime-main.8afd6130.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-Co-op/precache-manifest.c22f326011289535e560ca292e0e6dbc.js"
+  "/Covid-Co-op/precache-manifest.27ec1e2afdb4d2a947ff65ef607a7235.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Covid-Co-op/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
