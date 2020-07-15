8ca7c0d39bd1b27b39cb3f808739f92290055021
alexmaunder <alex_maunder@hotmail.com>
2020-07-16 01:57:51 +1000

    passed through user session to shopping cart page

--- src/components/App.js
+++ src/components/App.js
@@ -89,7 +89,7 @@ class App extends Component {
   handleLogin = (data) => {
     this.setState({
       isLoggedIn: true,
-      user: data.user //BUG doesnt set right? 
+      user: data.user //BUG doesnt set right?
     })
   }
 
@@ -123,8 +123,11 @@ class App extends Component {
                   <Route exact path='/sign-in' render={props => (
                     <Signin {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>)}
                   />{/* keep me at the bottom */}
-                  <Route exact path='/order' render={props => (<Order users={this.state.users} products={this.state.products} />)} />      {/* keep me at the bottom */}
-                  <Route exact path='/cart' component={Cart}/>
+                  <Route exact path='/order' render={props => (
+                    <Order users={this.state.users} products={this.state.products} />)} />{/* keep me at the bottom */}
+                  <Route exact path='/shoppingcart' render={props => (
+                    <ShoppingCart {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>)}
+                  />
                   <Route exact path='/profile' render={props => (
                     <MyProfile {...props} user={this.state.user} loggedInStatus={this.state.isLoggedIn} handleUserEdit={this.handleUserEdit}/>)}
                   />
