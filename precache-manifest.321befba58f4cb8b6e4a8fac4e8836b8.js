971fd37c95bba466a3c23aa091a7adb2b74ac640
Joshua McDonald <mcdojj12@gmail.com>
2020-07-15 12:43:51 +1000

    Updates

---
+++ precache-manifest.321befba58f4cb8b6e4a8fac4e8836b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c704c40e3ad10b2531349890ee33bab",
+    "url": "/Covid-Co-op/index.html"
+  },
+  {
+    "revision": "8eab529c1c3db6b90320",
+    "url": "/Covid-Co-op/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0316b0f0e4dac53cbd4f",
+    "url": "/Covid-Co-op/static/css/main.5aa34554.chunk.css"
+  },
+  {
+    "revision": "8eab529c1c3db6b90320",
+    "url": "/Covid-Co-op/static/js/2.a74fc8a9.chunk.js"
+  },
+  {
+    "revision": "a43dc87748136f1e31bd8faae01dda0d",
+    "url": "/Covid-Co-op/static/js/2.a74fc8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0316b0f0e4dac53cbd4f",
+    "url": "/Covid-Co-op/static/js/main.eef2a7b0.chunk.js"
+  },
+  {
+    "revision": "d3caf6ec0003b1ad318d",
+    "url": "/Covid-Co-op/static/js/runtime-main.8afd6130.js"
+  }
+]);
