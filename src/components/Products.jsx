28cb40a0399c5dc37ce8826202f9af76a87b150b
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-07-15 23:23:19 +1000

    Products sort and categories

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
-import _ from "lodash";
+import _, { uniq } from "lodash";
 import SearchResults from 'react-filter-search'
 import './Products.css'
 
 export class Products extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            value: ''
+            value: '',
+            products: [],
+            catagories: this.getCategories()
         }
 
         this.showProducts = this.showProducts.bind(this);
         this.matchUser = this.matchUser.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.getCategories = this.getCategories.bind(this);
     }
 
 
@@ -52,7 +55,7 @@ export class Products extends Component {
                         <p>{product.description.slice(0, 30)}...</p>
                             <p>Provided by: <a href="">{this.matchUser(product.user_id)}</a></p>
                         <p>Posted: {Math.floor(Math.abs(new Date() - new Date(product.created_at))/1000/60/60/24)} days ago</p>
-                        <button type="button" id="submit-btn" className="btn btn-success btn-sm" onClick={this.handleClick}>Add to Cart</button>
+                        <button type="button" className="btn btn-success btn-sm" onClick={this.handleClick}>Add to Cart</button>
                     </div>
                     )
                 })
@@ -62,11 +65,44 @@ export class Products extends Component {
         }
     }
 
+    getCategories() {
+        let allCategories = [];
+        if (this.props.products){
+
+            this.props.products.map((product) => {
+                allCategories.push(product.category);
+            });
+            let unique = allCategories.filter((item, i, ar) => ar.indexOf(item) === i);
+            return unique.map((category) => {
+                return (<option value={category}> {category} </option>)
+            });
+        }
+    }
+
     render() {
         return (
             <div className='row'>
                 <div className="row">
-                    <span className="search-heading">{"Search:"}</span> <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <div className="col-8 searchbar">
+                        <span className="search-heading">{"Search:"}</span> <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </div>
+                    <div className="col-4 sort">
+                        <div className="cat-select">
+                            <span className="category-heading">{"Category:"}</span>
+                            <select id="categories" name="categories">
+                                {this.getCategories()}
+                                <option value="all">None</option>
+                            </select>
+                        </div>
+                        <div className="sort-select">
+                            <span className="sort-heading">{"Sort by:"}</span>
+                            <select id="sort" name="sort">
+                                <option value="created-at">Date Added</option>
+                                <option value="num-orders">Popularity</option>
+                                <option value="quantity">Quantity Available</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                 <div className="row">
                     {this.showProducts()}
