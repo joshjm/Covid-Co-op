8b1e996dbd3007f2e0e1820438c066165e8db56f
Joshua McDonald <mcdojj12@gmail.com>
2020-07-15 11:49:14 +1000

    changed to hash router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ class NavBar extends Component{
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
-                <a className="navbar-brand" href="/">Covid Coop⚕</a>
+                <Link to="/" className="navbar-brand">Covid Coop⚕</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
