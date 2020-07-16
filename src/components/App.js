494c11ffefc0eadedf79736bbd8539e860bc16ba
alexmaunder <alex_maunder@hotmail.com>
2020-07-17 01:50:00 +1000

    working on user products display

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,6 @@ import ProductView from './ProductView';
 
 
 import './App.css';
-import AuthApi from './AuthApi';
 
 import {
   // BrowserRouter as Router,
@@ -165,7 +164,7 @@ class App extends Component {
                     <MyProfile user={this.state.user} loggedInStatus={this.state.isLoggedIn} handleUserEdit={this.handleUserEdit}/>)}
                   />
                   <Route exact path='/my-products' render={props => (
-                    <MyProducts user={this.state.user} loggedInStatus={this.state.isLoggedIn} />)}
+                    <MyProducts {...props} user={this.state.user} loggedInStatus={this.state.isLoggedIn} />)}
                   />
                   <Route exact path='/productview' render={props => (
                   <ProductView users={this.state.users} products={this.state.products} productSelect={this.state.productSelect} isLoggedIn={this.state.isLoggedIn} />)}
