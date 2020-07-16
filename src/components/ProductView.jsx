171214a33aae362caf749b14d76dd31f9ee87882
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-07-16 19:28:18 +1000

    product page links

---
+++ src/components/ProductView.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class ProductView extends Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ProductView
